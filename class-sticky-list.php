8efb6c1b685ba1950e85d13c7ca79bbcbb9a98d4
13pixlar <adam@13pixlar.se>
2015-05-18 09:26:38 +0200

    Show duplicate to anyone

--- class-sticky-list.php
+++ class-sticky-list.php
@@ -553,7 +553,7 @@ if (class_exists("GFForms")) {
                                     }
                                 }
 
-                                // Only show post link if postlink is enabled and
+                                // Only show post link if postlink is enabled
                                 if($enable_postlink && $entry["post_id"] != NULL) {
 
                                     $permalink = get_permalink($entry["post_id"]);
@@ -563,15 +563,12 @@ if (class_exists("GFForms")) {
                                 // Only show duplicate link if duplicate is enabled
                                 if($enable_duplicate) {
 
-                                    // ...and current user is the creator OR has the capability to publish posts
-                                    if($entry["created_by"] == $this->stickylist_get_current_user() || current_user_can('publish_posts')) {
-                                        $list_html .= "
-                                            <form action='$embedd_page' method='post'>
-                                                <button class='sticky-list-duplicate submit'>$enable_duplicate_label</button>
-                                                <input type='hidden' name='mode' value='duplicate'>
-                                                <input type='hidden' name='duplicate_id' value='$entry_id'>
-                                            </form>";
-                                    }
+                                    $list_html .= "
+                                        <form action='$embedd_page' method='post'>
+                                            <button class='sticky-list-duplicate submit'>$enable_duplicate_label</button>
+                                            <input type='hidden' name='mode' value='duplicate'>
+                                            <input type='hidden' name='duplicate_id' value='$entry_id'>
+                                        </form>";
                                 }
 
                             $list_html .= "</td>";
