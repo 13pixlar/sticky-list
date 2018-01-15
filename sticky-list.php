f8075ef48fb4d060c71901bb7d4cca9d3d5e4e3c
Adam Rehal <adam.rehal@nineyards.se>
2018-01-15 11:45:27 +0100

    Added insights functionality

--- sticky-list.php
+++ sticky-list.php
@@ -14,6 +14,37 @@ Author URI: http://13pixlar.se
  */
 
 //------------------------------------------
+
+// Create a helper function for easy SDK access.
+function stickylist_fs() {
+    global $stickylist_fs;
+
+    if ( ! isset( $stickylist_fs ) ) {
+        // Include Freemius SDK.
+        require_once dirname(__FILE__) . '/includes/start.php';
+
+        $stickylist_fs = fs_dynamic_init( array(
+            'id'                  => '1528',
+            'slug'                => 'gravity-forms-sticky-list',
+            'type'                => 'plugin',
+            'public_key'          => 'pk_ab96a4a2bac76862a81de1dde21a9',
+            'is_premium'          => false,
+            'has_addons'          => false,
+            'has_paid_plans'      => false,
+            'menu'                => array(
+                'first-path'     => 'plugins.php',
+            ),
+        ) );
+    }
+
+    return $stickylist_fs;
+}
+
+// Init Freemius.
+stickylist_fs();
+// Signal that SDK was initiated.
+do_action( 'stickylist_fs_loaded' );
+
 add_action( 'gform_loaded', array( 'StickyList_Bootstrap', 'load' ), 5 );
 class StickyList_Bootstrap {
 
